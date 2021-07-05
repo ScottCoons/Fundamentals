d6b66d8a0a3fd092e9c43cd393abcf11d769a41e
Scott Coons <scoons@cnm.edu>
2021-07-04 18:32:18 -0600

    initial commit

---
+++ objects-apis.js
@@ -0,0 +1,22 @@
+/*
+ * Objects, Interfaces, and APIs.
+ */
+
+let dog= {
+    name: "Steve",
+    color: "brown",
+    breed: "Pug",
+    size: "Small",
+    bark: function(typeOfBark) {
+        console.log("Bark!");
+    }
+}
+
+/*
+ * Passing data by value, and reference.
+ */
+
+let y = 5;
+function x(y) {
+
+}
